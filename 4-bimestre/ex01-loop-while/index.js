e7a254a3e5ac1a7f32a509254ee509fbcdddb80d
luis-fernando-braga-dos-santos <luis.santos295@aluno.ce.gov.br>
2025-10-09 22:00:32 +0000

    Corrigido!

--- 4-bimestre/ex01-loop-while/index.js
+++ 4-bimestre/ex01-loop-while/index.js
@@ -1,4 +1,14 @@
 // CRIE UMA LÓGICA AQUI
 
+let contador = 1
+let resultado
+
+while(contador <= 10){
+    resultado = contador
+    contador = contador += 1
+}
+
+console.log(`resultado:${resultado}`)
+
 // === NÃO MODIFIQUE NADA ABAIXO ============
 module.exports = resultado

3686ba0859be9ba2c8b4ade26495ddb96b37ac72
luis-fernando-braga-dos-santos <luis.santos295@aluno.ce.gov.br>
2025-10-21 14:38:15 +0000

    projeto 6 atualizado !

--- 4-bimestre/ex06-loop-array/index.js
+++ 4-bimestre/ex06-loop-array/index.js
@@ -1,4 +1,11 @@
 // CRIE UMA LÓGICA AQUI
 
+let filmes = ["Vingadores", "Homem-Aranha", "Interestelar", "Matrix"]
+
+for (let i = 0; i < filmes.length; i++) {
+    console.log(`Filme ${i + 1}: ${filmes[i]}`)
+}
+
+
 // === NÃO MODIFIQUE OU ADICIONE NADA ABAIXO ============
 module.exports = { filmes }

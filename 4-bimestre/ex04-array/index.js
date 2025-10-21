9c995e4a7cd062b667666c75a2e8de7a7cf04868
luis-fernando-braga-dos-santos <luis.santos295@aluno.ce.gov.br>
2025-10-21 14:34:01 +0000

    projeto 4 atualizado !

--- 4-bimestre/ex04-array/index.js
+++ 4-bimestre/ex04-array/index.js
@@ -1,4 +1,12 @@
 // CRIE UMA LÓGICA AQUI
 
+let materias = ["Matemática", "Português", "História", "Ciências"]
+
+console.log("Primeira matéria:", materias[0])
+
+console.log("Última matéria:", materias[materias.length - 1])
+
+console.log("Total de matérias:", materias.length)
+
 // === NÃO MODIFIQUE OU ADICIONE NADA ABAIXO ============
 module.exports = { materias }

e6b324bb741183a2ba77bf034cb738442f1185df
luis-fernando-braga-dos-santos <luis.santos295@aluno.ce.gov.br>
2025-10-30 17:45:14 +0000

    Atualizado!

--- 4-bimestre/ex10-acessando-e-alterando-obj/index.js
+++ 4-bimestre/ex10-acessando-e-alterando-obj/index.js
@@ -1,10 +1,19 @@
 // CRIE UMA LÓGICA AQUI
 
+const carro = {
+    marca:"toyota",
+    modelo:"corolla",
+    ano:2020,
+    cor:"prata"
+}
 
+console.log(carro)
 
+carro.cor = 'Preto';
 
+carro.preco = 120000;
 
-
+console.log(carro);
 
 // === NÃO MODIFIQUE OU ADICIONE NADA ABAIXO ============
 module.exports = { carro }
